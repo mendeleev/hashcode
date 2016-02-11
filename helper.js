4b8b4db6549591a03ce4e13d28a8b2fdd583da87
zagnit <riftersound@gmail.com>
2016-02-11 21:13:15 +0200

    add

--- helper.js
+++ helper.js
@@ -3,28 +3,41 @@
 let fs = require("fs"),
     co = require("co"),
     Q = require("q"),
-    _ = require('underscore')
-
+    _ = require('underscore'),
+    map = {
+      r: 0,
+      c: 0
+    }
+    ;
 
 co(function * () {
+  function calculStep(from,to){
+    var x=from.r-to.r;
+    return ;
+  }
   /**2x2
-   * [
-   * [[],[]],
-   * [[],[]]
-   * ]
+   *
    * @type {{buildMap: module.exports.buildMap}}
    */
   module.exports = {
-    buildMap: function (x, y) {
-      var results = [];
-      for (let i = 0; i < x; i++) {
-        let row = [];
-        for (let j = 0; j < y; j++) {
-          row.push([]);
-        }
-        results.push(row);
-      }
+    setMap: function (r, c) {
+      map.r = r;
+      map.c = c;
+    },
+    send: function (dron, warehouse, customer, products) {
+      let dron = {
+            r: 0,
+            c: 0,
+            load: 500,
+        items
+          },
+          warehouse = {
+            r: 0,
+            c: 0,
+            items :[]
+          }
     }
+
   }
 
 
