1750dde108e0ad7e930b5ca89dc96c3f3215b245
priadko <andrii.priadko@ria.com>
2016-02-11 19:05:19 +0200

    init method added

--- app/paint.js
+++ app/paint.js
@@ -5,13 +5,18 @@
   var paint = {
     tile: 10,
 
+    init: function(width, height) {
+      canvas.width = width || 800;
+      canvas.height = height || 600;
+    },
+
     point: function(x, y, color) {
       ctx.fillStyle = color;
       ctx.fillRect(x*this.tile, y*this.tile, this.tile, this.tile);
     }
   };
 
-
+  paint.init();
 
 
 })();
