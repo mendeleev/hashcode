38f5a21502d83a0b1f8f10755d247ce1f46cb613
SamhainG <gavrilyk.oleg@gmail.com>
2016-02-11 20:10:40 +0200

    first files

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 "use strict";
 
 let fs = require("fs"),
+    co = require("co"),
     Q = require("q"),
     parser = require("./parser");
 
 
 co(function * () {
-
+  let content = yield Q.nfcall(fs.readFile, process.argv.pop());
+  parser(content.toString());
 });
 
