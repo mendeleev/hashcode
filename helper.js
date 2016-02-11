f0f9a59dcdb673d535bd46431c39df73a8a65c28
zagnit <riftersound@gmail.com>
2016-02-11 22:21:41 +0200

    add

--- helper.js
+++ helper.js
@@ -10,7 +10,7 @@ let fs = require("fs"),
     },
     commands = [],
     steps = 1000,
-    limits=500
+    limits = 500
     ;
 const warehouse = {
   r: 0,
@@ -22,45 +22,49 @@ co(function * () {
   var all = yield  Q.nfcall(fs.readFile, '/var/www/hashcode/app/files/parsed/mother_of_all_warehouses.in', 'utf-8');
   console.log("=================");
   console.log(JSON.parse(all));
-  function move(from, to,dron) {
-    if ((steps - 1) > 0) {
+  function move(from, to, dron) {
+    if ((steps - 1) > 0 && ((dron.turns - 1) > 0)) {
       commands.push({
         length: Math.floor(Math.sqrt(Math.pow((from.r - to.r), 2) + Math.pow((from.c - to.c), 2))),
         from: from,
         to: to
       });
-      dron.r=to.r;
-      dron.c=to.c;
+      dron.r = to.r;
+      dron.c = to.c;
       steps -= Math.floor(Math.sqrt(Math.pow((from.r - to.r), 2) + Math.pow((from.c - to.c), 2)));
+      dron.turns--;
     } else {
       throw new Error;
     }
   }
 
   function load(dron, order) {
-    if ((steps - 1) > 0) {
+    if (((steps - 1) > 0) && ((dron.turns - 1) > 0)) {
       while (dron.limit) {
-        let oneOrder = order.shift();
         warehouse.items.shift();
-        dron.items.push(oneOrder);
-        dron.limit -= oneOrder;
+        dron.items.push(order);
+        dron.limit -= order;
+
       }
+      dron.turns--;
       steps--;
     } else {
       throw new Error;
     }
 
   }
-  function unload(client,dron){
-    if ((steps - 1) > 0) {
+
+  function unload(client, dron) {
+    if ((steps - 1) > 0&&((dron.turns - 1) > 0)) {
       while (dron.limit) {
-        client.items=union(client.items,dron.items);
-        dron.items=[];
+        client.items = union(client.items, dron.items);
+        dron.items = [];
         dron.limit = limits;
       }
-      dron.r=client.r;
-      dron.c=client.c;
+      dron.r = client.r;
+      dron.c = client.c;
       steps--;
+      dron.turns--;
     } else {
       throw new Error;
     }
@@ -86,7 +90,9 @@ co(function * () {
         r: 0,
         c: 0,
         items: [10, 20],
-      };
+      }
+          order=[100,100,200]
+
       while (steps > 0) {
         if (dron.c != client.c && dron.r != client.r) {
           if (dron.c != warehouse.c && dron.r != warehouse.r) {
@@ -114,7 +120,7 @@ co(function * () {
             )
           }
         } else {
-          unload(client,dron);
+          unload(client, dron);
           move({
                 c: dron.c,
                 r: dron.r
